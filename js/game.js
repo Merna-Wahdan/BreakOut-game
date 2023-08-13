7f1d21f2e90864cc276a0d1a421124c362a78257
Merna <merna.wahdan@gmail.com>
2023-08-13 13:58:59 +0200

    Create Ball class

--- js/game.js
+++ js/game.js
@@ -16,9 +16,30 @@ class Paddle {
         cTxt.fillStyle = "red";
         cTxt.fillRect(x, this.y, this.width, this.height)
         cTxt.stroke();
+        cTxt.closePath();
     }
 }
 
+class Ball {
+    constructor() {
+        this.r = 15
+        this.x = canvas.width/2 
+        // this.y = canvas.height - this.r - paddle.height
+        this.y = paddle.y - this.r
+    }
+
+    draw() {
+        cTxt.beginPath();
+        cTxt.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        cTxt.fillStyle = "green"
+        cTxt.fill()
+        cTxt.stroke()
+        cTxt.closePath()
+    }
+}
 
 const paddle = new Paddle()
-paddle.draw(150)
+paddle.draw(canvas.width/2 - paddle.width/2)
+
+const ball = new Ball()
+ball.draw()
